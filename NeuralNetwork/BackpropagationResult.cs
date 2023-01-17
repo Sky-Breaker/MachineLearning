967e0adddc5533b509d7557c8b015ae81648d006
Sky-Breaker <ffpilot22@outlook.com>
2023-01-16 19:39:49 -0800

    Created BackpropagationResult class to contain a network and an error value

---
+++ NeuralNetwork/BackpropagationResult.cs
@@ -0,0 +1,14 @@
+﻿namespace NeuralNetwork
+{
+    public class BackpropagationResult
+    {
+        public NetworkGradient Gradient;
+        public float Error;
+
+        public BackpropagationResult(NetworkGradient gradient, float error)
+        {
+            Gradient = gradient;
+            Error = error;
+        }
+    }
+}
