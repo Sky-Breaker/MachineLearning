2d38c004772ea83034997d89e73f7acf859c9fcb
Sky-Breaker <ffpilot22@outlook.com>
2023-02-14 20:05:08 -0800

    Add GPU acceleration C++ project and connect build output to main C# project

--- NvidiaGPUAcceleration/dllmain.cpp
+++ NvidiaGPUAcceleration/dllmain.cpp
@@ -17,5 +17,3 @@ BOOL APIENTRY DllMain( HMODULE hModule,
     return TRUE;
 }
 
-
-
--- NvidiaGPUAcceleration/nvidiagpu_matrixaccel.cpp
+++ NvidiaGPUAcceleration/nvidiagpu_matrixaccel.cpp
@@ -1,1 +1,11 @@
+#include "pch.h"
 #include <cublas_v2.h>
+
+extern "C" int initializeGPUMatrixAcceleration() {
+	cublasHandle_t cublasHandle;
+	cublasStatus_t status = cublasCreate(&cublasHandle);
+	bool initSuccessful = status == CUBLAS_STATUS_SUCCESS;
+	cublasDestroy(cublasHandle);
+	//return initSuccessful;
+	return 1;
+}
