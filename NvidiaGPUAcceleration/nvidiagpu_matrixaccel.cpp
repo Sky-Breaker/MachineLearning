b4d9721e67529b83e52c971eb01dd640c4eeb870
Sky-Breaker <ffpilot22@outlook.com>
2023-02-17 20:08:20 -0800

    Add declspec modifier to C++ function

--- NvidiaGPUAcceleration/nvidiagpu_matrixaccel.cpp
+++ NvidiaGPUAcceleration/nvidiagpu_matrixaccel.cpp
@@ -1,11 +1,11 @@
 #include "pch.h"
 #include <cublas_v2.h>
 
-extern "C" int initializeGPUMatrixAcceleration() {
+extern "C" __declspec(dllexport) int initializeGPUMatrixAcceleration() {
 	cublasHandle_t cublasHandle;
 	cublasStatus_t status = cublasCreate(&cublasHandle);
 	bool initSuccessful = status == CUBLAS_STATUS_SUCCESS;
 	cublasDestroy(cublasHandle);
 	//return initSuccessful;
-	return 1;
+	return 100;
 }
